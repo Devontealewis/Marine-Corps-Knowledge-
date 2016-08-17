22f6510bcb2755f626240c2ca8801ca3a4e62a4a
devontealewis <dl01757@georgiasouthern.edu>
2016-08-17 11:18:28 -0400

    coding

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -31,6 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.M16pictureBox = new System.Windows.Forms.PictureBox();
+            this.SawpictureBox = new System.Windows.Forms.PictureBox();
+            this.M240pictureBox = new System.Windows.Forms.PictureBox();
+            this.M2pictureBox = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.btnSelect = new System.Windows.Forms.Button();
             this.MK19pictureBox = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -39,24 +44,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtboxweight = new System.Windows.Forms.TextBox();
+            this.txtboxfirerate = new System.Windows.Forms.TextBox();
+            this.txtboxeffrange = new System.Windows.Forms.TextBox();
+            this.txtboxmaxrange = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.label5 = new System.Windows.Forms.Label();
-            this.M2pictureBox = new System.Windows.Forms.PictureBox();
-            this.M240pictureBox = new System.Windows.Forms.PictureBox();
-            this.SawpictureBox = new System.Windows.Forms.PictureBox();
-            this.M16pictureBox = new System.Windows.Forms.PictureBox();
+            this.M4pictureBox = new System.Windows.Forms.PictureBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.M16pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SawpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M240pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M2pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.MK19pictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.M2pictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.M240pictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.SawpictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.M16pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M4pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -71,6 +73,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.M4pictureBox);
             this.tabPage1.Controls.Add(this.M16pictureBox);
             this.tabPage1.Controls.Add(this.SawpictureBox);
             this.tabPage1.Controls.Add(this.M240pictureBox);
@@ -83,10 +86,10 @@
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.dataGridView1);
-            this.tabPage1.Controls.Add(this.textBox4);
-            this.tabPage1.Controls.Add(this.textBox3);
-            this.tabPage1.Controls.Add(this.textBox2);
-            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.txtboxweight);
+            this.tabPage1.Controls.Add(this.txtboxfirerate);
+            this.tabPage1.Controls.Add(this.txtboxeffrange);
+            this.tabPage1.Controls.Add(this.txtboxmaxrange);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -94,7 +97,57 @@
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Weapon Systems";
             this.tabPage1.UseVisualStyleBackColor = true;
-    
+            // 
+            // M16pictureBox
+            // 
+            this.M16pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("M16pictureBox.Image")));
+            this.M16pictureBox.Location = new System.Drawing.Point(249, 29);
+            this.M16pictureBox.Name = "M16pictureBox";
+            this.M16pictureBox.Size = new System.Drawing.Size(397, 194);
+            this.M16pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.M16pictureBox.TabIndex = 16;
+            this.M16pictureBox.TabStop = false;
+            this.M16pictureBox.Visible = false;
+            // 
+            // SawpictureBox
+            // 
+            this.SawpictureBox.Image = ((System.Drawing.Image)(resources.GetObject("SawpictureBox.Image")));
+            this.SawpictureBox.Location = new System.Drawing.Point(249, 29);
+            this.SawpictureBox.Name = "SawpictureBox";
+            this.SawpictureBox.Size = new System.Drawing.Size(397, 194);
+            this.SawpictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SawpictureBox.TabIndex = 15;
+            this.SawpictureBox.TabStop = false;
+            // 
+            // M240pictureBox
+            // 
+            this.M240pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("M240pictureBox.Image")));
+            this.M240pictureBox.Location = new System.Drawing.Point(249, 29);
+            this.M240pictureBox.Name = "M240pictureBox";
+            this.M240pictureBox.Size = new System.Drawing.Size(397, 194);
+            this.M240pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.M240pictureBox.TabIndex = 14;
+            this.M240pictureBox.TabStop = false;
+            // 
+            // M2pictureBox
+            // 
+            this.M2pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("M2pictureBox.Image")));
+            this.M2pictureBox.Location = new System.Drawing.Point(249, 29);
+            this.M2pictureBox.Name = "M2pictureBox";
+            this.M2pictureBox.Size = new System.Drawing.Size(397, 194);
+            this.M2pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.M2pictureBox.TabIndex = 13;
+            this.M2pictureBox.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(402, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Name";
             // 
             // btnSelect
             // 
@@ -104,6 +157,7 @@
             this.btnSelect.TabIndex = 11;
             this.btnSelect.Text = "Select";
             this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
             // 
             // MK19pictureBox
             // 
@@ -162,39 +216,40 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(144, 326);
             this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Column1
             // 
             this.Column1.HeaderText = "Weapons ";
             this.Column1.Name = "Column1";
             // 
-            // textBox4
+            // txtboxweight
             // 
-            this.textBox4.Location = new System.Drawing.Point(336, 342);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(310, 20);
-            this.textBox4.TabIndex = 4;
+            this.txtboxweight.Location = new System.Drawing.Point(336, 342);
+            this.txtboxweight.Name = "txtboxweight";
+            this.txtboxweight.Size = new System.Drawing.Size(310, 20);
+            this.txtboxweight.TabIndex = 4;
             // 
-            // textBox3
+            // txtboxfirerate
             // 
-            this.textBox3.Location = new System.Drawing.Point(336, 306);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(310, 20);
-            this.textBox3.TabIndex = 3;
+            this.txtboxfirerate.Location = new System.Drawing.Point(336, 306);
+            this.txtboxfirerate.Name = "txtboxfirerate";
+            this.txtboxfirerate.Size = new System.Drawing.Size(310, 20);
+            this.txtboxfirerate.TabIndex = 3;
             // 
-            // textBox2
+            // txtboxeffrange
             // 
-            this.textBox2.Location = new System.Drawing.Point(336, 267);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(310, 20);
-            this.textBox2.TabIndex = 2;
+            this.txtboxeffrange.Location = new System.Drawing.Point(336, 267);
+            this.txtboxeffrange.Name = "txtboxeffrange";
+            this.txtboxeffrange.Size = new System.Drawing.Size(310, 20);
+            this.txtboxeffrange.TabIndex = 2;
             // 
-            // textBox1
+            // txtboxmaxrange
             // 
-            this.textBox1.Location = new System.Drawing.Point(336, 229);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(310, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtboxmaxrange.Location = new System.Drawing.Point(336, 229);
+            this.txtboxmaxrange.Name = "txtboxmaxrange";
+            this.txtboxmaxrange.Size = new System.Drawing.Size(310, 20);
+            this.txtboxmaxrange.TabIndex = 1;
             // 
             // tabPage2
             // 
@@ -206,56 +261,16 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // label5
+            // M4pictureBox
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(402, 3);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(72, 25);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Name";
-            // 
-            // M2pictureBox
-            // 
-            this.M2pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("M2pictureBox.Image")));
-            this.M2pictureBox.Location = new System.Drawing.Point(249, 29);
-            this.M2pictureBox.Name = "M2pictureBox";
-            this.M2pictureBox.Size = new System.Drawing.Size(397, 194);
-            this.M2pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.M2pictureBox.TabIndex = 13;
-            this.M2pictureBox.TabStop = false;
-            // 
-            // M240pictureBox
-            // 
-            this.M240pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("M240pictureBox.Image")));
-            this.M240pictureBox.Location = new System.Drawing.Point(249, 29);
-            this.M240pictureBox.Name = "M240pictureBox";
-            this.M240pictureBox.Size = new System.Drawing.Size(397, 194);
-            this.M240pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.M240pictureBox.TabIndex = 14;
-            this.M240pictureBox.TabStop = false;
-            // 
-            // SawpictureBox
-            // 
-            this.SawpictureBox.Image = ((System.Drawing.Image)(resources.GetObject("SawpictureBox.Image")));
-            this.SawpictureBox.Location = new System.Drawing.Point(249, 29);
-            this.SawpictureBox.Name = "SawpictureBox";
-            this.SawpictureBox.Size = new System.Drawing.Size(397, 194);
-            this.SawpictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.SawpictureBox.TabIndex = 15;
-            this.SawpictureBox.TabStop = false;
-            // 
-            // M16pictureBox
-            // 
-            this.M16pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("M16pictureBox.Image")));
-            this.M16pictureBox.Location = new System.Drawing.Point(249, 29);
-            this.M16pictureBox.Name = "M16pictureBox";
-            this.M16pictureBox.Size = new System.Drawing.Size(397, 194);
-            this.M16pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.M16pictureBox.TabIndex = 16;
-            this.M16pictureBox.TabStop = false;
-            this.M16pictureBox.Visible = false;
+            this.M4pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("M4pictureBox.Image")));
+            this.M4pictureBox.Location = new System.Drawing.Point(249, 29);
+            this.M4pictureBox.Name = "M4pictureBox";
+            this.M4pictureBox.Size = new System.Drawing.Size(397, 194);
+            this.M4pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.M4pictureBox.TabIndex = 17;
+            this.M4pictureBox.TabStop = false;
+            this.M4pictureBox.Visible = false;
             // 
             // Form1
             // 
@@ -269,12 +284,13 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.M16pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SawpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M240pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M2pictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.MK19pictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.M2pictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.M240pictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.SawpictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.M16pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M4pictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -285,10 +301,10 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtboxweight;
+        private System.Windows.Forms.TextBox txtboxfirerate;
+        private System.Windows.Forms.TextBox txtboxeffrange;
+        private System.Windows.Forms.TextBox txtboxmaxrange;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.PictureBox MK19pictureBox;
         private System.Windows.Forms.Label label4;
@@ -301,6 +317,7 @@
         private System.Windows.Forms.PictureBox M16pictureBox;
         private System.Windows.Forms.PictureBox SawpictureBox;
         private System.Windows.Forms.PictureBox M240pictureBox;
+        private System.Windows.Forms.PictureBox M4pictureBox;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,6 +12,7 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        String WpnName;
         public Form1()
         {
             InitializeComponent();
@@ -24,14 +25,77 @@ namespace WindowsFormsApplication1
             this.dataGridView1.Rows.Add("M240");
             this.dataGridView1.Rows.Add("Saw");
             this.dataGridView1.Rows.Add("M16");
+            this.dataGridView1.Rows.Add("M4");
             MK19pictureBox.Visible = false;
             M2pictureBox.Visible = false;
             M240pictureBox.Visible = false;
             SawpictureBox.Visible = false;
-            M16pictureBox.Visible = false; 
-
+            M16pictureBox.Visible = false;
+            M4pictureBox.Visible = false;
+            label5.Visible = false; 
 
             
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            {
+                if (WpnName == "Mk19")
+                {
+                    MK19pictureBox.Visible = true;
+                    M2pictureBox.Visible = false;
+                    M240pictureBox.Visible = false;
+                    SawpictureBox.Visible = false;
+                    M16pictureBox.Visible = false;
+                    M4pictureBox.Visible = false;
+                    label5.Visible = false;
+
+                    txtboxmaxrange.Text = "2,212m";
+                    txtboxeffrange.Text = "1,500m";
+                    txtboxfirerate.Text = "40 rounds/min sustained 60 rounds/min rapid";
+                    txtboxweight.Text = "77.6 lbs";
+                }
+                if (WpnName == "M2 50 cal") 
+                {
+                    MK19pictureBox.Visible = false;
+                    M2pictureBox.Visible = true;
+                    M240pictureBox.Visible = false;
+                    SawpictureBox.Visible = false;
+                    M16pictureBox.Visible = false;
+                    M4pictureBox.Visible = false;
+                    label5.Visible = false;
+
+                    txtboxmaxrange.Text = "2,000m";
+                    txtboxeffrange.Text = "1,830m area, 1,500m point";
+                    txtboxfirerate.Text = "450-575 rounds per minute";
+                    txtboxweight.Text = "84 lbs";
+                
+                }
+
+                if (WpnName == "M240")
+                {
+                    MK19pictureBox.Visible = false;
+                    M2pictureBox.Visible = false;
+                    M240pictureBox.Visible = true;
+                    SawpictureBox.Visible = false;
+                    M16pictureBox.Visible = false;
+                    M4pictureBox.Visible = false;
+                    label5.Visible = false;
+
+                    txtboxmaxrange.Text = "3,725m";
+                    txtboxeffrange.Text = "800m bipod, 1,100m tripod";
+                    txtboxfirerate.Text = "750-950 rounds per minute";
+                    txtboxweight.Text = "27.6 lbs";
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            DataGridViewRow row = dataGridView1.Rows[rowIndex];
+
+            WpnName = dataGridView1.Rows[rowIndex].Cells[0].Value.ToString();
         }
 
     
